161735d0bcf97e202a396467f06639b062306794
Ben Stone <benjamin.h.stone@gmail.com>
2021-02-12 12:41:18 -0500

    Initial Commit

---
+++ AddressBook/AddressBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressBookApp.swift
+//  AddressBook
+//
+//  Created by Ben Stone on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AddressBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
